0346bf19ddfbcbd2ce09daab905d2ea8b5e704be
王建军 <wangjj1011@gmail.com>
2017-09-18 01:16:51 +0800

    practice done

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -48,11 +48,14 @@ public class Practice03OfObjectLayout extends RelativeLayout {
     }
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
+        PointF mPointF = new PointF();
 
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            mPointF.x = startValue.x + fraction * (endValue.x - startValue.x);
+            mPointF.y = startValue.y + fraction * (endValue.y - startValue.y);
+            return mPointF;
         }
     }
 }
